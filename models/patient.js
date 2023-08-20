2dcb293356c74d689de85696d61d4e7be338c0fc
raymanhuang_ <imarayman123@gmail.com>
2023-08-20 02:30:05 -0400

    added basic CRUD

---
+++ models/patient.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PatientSchema = new mongoose.Schema({
+    name: { type: String, unique: true, required: true },
+    age: Number,
+    gender: String,
+    diagnosisHistory: [{
+        image: { type: String },
+        prediction: {type: String}
+    }]
+});
+
+module.exports = mongoose.model('Patient', PatientSchema)
